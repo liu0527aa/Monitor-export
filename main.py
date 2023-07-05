c864c4b63f76bd257c232e9e03fd31562296921a
zhiyong233 <wzy133975@163.com>
2023-07-06 00:00:36 +0800

    wzy第一次提交

--- main.py
+++ main.py
@@ -5,44 +5,117 @@ import time
 import csv
 import requests
 import json
+
 # 创建 Chrome 浏览器实例
 CORP_ID = "wwc307dfc2cab1e8ed"
-
 SECRET = "q_ml2ucAQg_GG0PCE4T-eSPhkqKI-cU5tAZEQw7sHrg"
-driver = webdriver.Chrome()
-shoes="lv trainer 黑牛仔 4码"
+
+
+"""-------反爬设置------"""
+options = webdriver.ChromeOptions()
+options.add_experimental_option('excludeSwitches', ['enable-automation'])
+options.add_argument("--disable-blink-features=AutomationControlled")
+
+driver = webdriver.Chrome(options=options)
+driver.execute_cdp_cmd("Page.addScriptToEvaluateOnNewDocument", {
+    "source": """
+                Object.defineProperty(navigator, 'webdriver', {
+                  get: () => undefined
+                })
+              """
+})
+
+
 # 访问网页
-driver.get("https://it.louisvuitton.com/ita-it/prodotti/sneaker-lv-trainer-nvprod3150035v/1A9JGS")
-# p_input = driver.find_element(By.ID, 'key')
-# p_input.send_keys('python编程')  # 找到输入框输入
+driver.get("https://www.louisvuitton.cn/zhs-cn/homepage")
 time.sleep(1)
 # 点击搜素按钮
-sale_on=driver.find_element(By.CLASS_NAME,"ucm-popin-close-text").click()
+driver.find_element(By.CLASS_NAME, "ucm-button.ucm-button--default.ucm-choice__yes").click()
+driver.find_element(By.CLASS_NAME, "lv-button").click() # 点击搜索按钮
+element = driver.find_element_by_id('searchHeaderInput')
+element.send_keys("LV TRAINER")  # 输入搜索内容
+element.send_keys(Keys.ENTER)   # 回车
+time.sleep(2)
+driver.find_element_by_id('product-1ABLY1').click()  # 点击商品进去详情页
+time.sleep(2)
+# good_id = driver.find_element(By.CLASS_NAME, "lv-product__sku").text
+# good_name = driver.find_element(By.CLASS_NAME, "lv-product__name").text
+# good_price = driver.find_element(By.CLASS_NAME, "notranslate").text
+# good_style = driver.find_elements(By.CLASS_NAME, "lv-product-variation-selector__value")
+# # good_size = driver.find_element(By.CLASS_NAME, "lv-product-variation-selector__value").text
+# print(good_id)
+# print(good_name)
+# print(good_price)
+# print(good_style[0].text)
+# print(good_style[1].text)
+
 # time.sleep(1)
+
+# 鞋子款式list
+shoes = []
+
 all_book_info = []
 head=['书名', '价格']
 #csv文件的路径和名字
 path='./file/book.csv'
+
+
 def write_csv(head,all_book_info,path):
     with open(path, 'w', newline='',encoding='utf-8') as file:
         fileWriter = csv.writer(file)
         fileWriter.writerow(head)
         fileWriter.writerows(all_book_info)
+
+
 # 爬取一页
 def get_onePage_info():
-    # driver.execute_script('window.scrollTo(0, document.body.scrollHeight);')
-    time.sleep(2)
-    res=[]
-    # 书籍列表
-    sale = driver.find_element(By.CLASS_NAME, "lv-product-purchase__button").text
-    if sale == "Prodotto non disponibile":
-        res="无货"
-        print(res)
-    elif sale == "Aggiungi alla shopping bag":
-        res="有货"
-        print(res)
-    return res
-    # all_book_info.append(res)
+    time.sleep(1)
+    # 进入款式列表
+    driver.find_elements(By.CLASS_NAME, "lv-product-variation-selector__value")[0].click()
+    time.sleep(1)
+    # 获取款式list
+    items = driver.find_elements(By.CLASS_NAME, "lv-choice-group__item.lv-product-panel-grid__list-item")
+    length = items.__len__()
+    # 多款鞋尺码及库存
+    MulSize_stocks = [[] for i in range(length)]
+    for i in range(length):
+        # 依次点击不同款式
+        driver.find_elements(By.CLASS_NAME, "lv-choice-group__item.lv-product-panel-grid__list-item")[i].click()
+        time.sleep(1)
+        # 获取商品信息
+        name = driver.find_element(By.CLASS_NAME, "lv-product__name").text
+        color = driver.find_elements(By.CLASS_NAME, "lv-product-variation-selector__value")[0].text
+        shoes.append(f"{name}--{color}")
+        one_stocks = get_sizeStock_info()
+        MulSize_stocks[i].append(one_stocks)
+        if i == items.__len__()-1:
+            break
+        driver.find_elements(By.CLASS_NAME, "lv-product-variation-selector__value")[0].click()
+        time.sleep(1)
+
+    return MulSize_stocks
+
+
+def get_sizeStock_info():
+    # 进入尺码列表查看库存
+    time.sleep(1)
+    driver.find_elements(By.CLASS_NAME, "lv-product-variation-selector__value")[1].click()
+    time.sleep(1)
+    sizes = driver.find_elements(By.CLASS_NAME, "lv-product-panel-list__item-name")
+    stocks = driver.find_elements(By.CLASS_NAME, "lv-product-panel-list__item-status")
+    size_stocks = []
+    for i in range(sizes.__len__()):
+        if stocks[i].text == "":
+            size_stocks.append(f"{sizes[i].text}--可选购")
+        else:
+            size_stocks.append(f"{sizes[i].text}--{stocks[i].text}")
+    time.sleep(1)
+    # 关闭尺码表
+    driver.find_element(By.CLASS_NAME, "lv-modal__close.lv-icon-button.-light").click()
+    time.sleep(1)
+    return size_stocks
+
+
 class WeChatPub:
     s = requests.session()
     token = None
@@ -60,9 +133,9 @@ class WeChatPub:
             print("request failed.")
             return None
 
-    def send_msg(self, content,res):
+    def send_msg(self, content, res):
         url = "https://qyapi.weixin.qq.com/cgi-bin/message/send?access_token=" + self.token
-        url = f"https://sctapi.ftqq.com/SCT214361T7tWl9uKZfNvPkiCy2SUi19so.send?title={shoes} is {res}"
+        url = f"https://sctapi.ftqq.com/SCT214982TPYjiDoACeUsOEsCeOpvpUWbR.send?title={shoes[0]} is {res[0]}"
         header = {
             "Content-Type": "application/json"
         }
@@ -80,7 +153,6 @@ class WeChatPub:
             return json.loads(rep.content)
         else:
             print("request failed.")
-
 res = get_onePage_info()
 # driver.find_element(By.CLASS_NAME, 'pn-next').click()  # 点击下一页
 time.sleep(2)
